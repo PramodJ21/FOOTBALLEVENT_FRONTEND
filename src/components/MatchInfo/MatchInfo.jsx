a001bdea264bf16192e29f7177d27862fd764ba1
JOSHI PRAMOD SANDEEP AKALPITA <95874710+PramodJ21@users.noreply.github.com>
2025-05-06 19:20:08 +0530

    Update MatchInfo.jsx

--- src/components/MatchInfo/MatchInfo.jsx
+++ src/components/MatchInfo/MatchInfo.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
 import './MatchInfo.css';
+import { FaCalendarAlt, FaClock, FaDoorOpen } from "react-icons/fa";
 
 const MatchInfo = () => (
   <section className="match-info">
     <div className="teams">
-      <div className="team team-barca">
-        <img src="/barca_logo.png" alt="Barcelona Logo" className="team-logo" />
+      <div className="team team-barcelona">
+        <img src="/barca_logo.png" alt="Barcelona Flag" className="team-logo barcelona-logo"/>
+        <span className="team-name">FC Barcelona</span>
       </div>
+      
+      <div className="match-details-wrapper">
+      <div className="vs-text">VS</div>
+      
       <div className="match-details">
         <h1 className="playwrite">
-          <span>FC Barcelona vs Inter Milan</span><br />
-          <span>UCL - 2<sup>nd</sup> Leg</span>
+          <span>El Clásico</span>
         </h1>
       </div>
-      <div className="team team-milan">
-        <img src="/milan_logo.png" alt="Inter Milan Logo" className="team-logo" />
+      </div>
+
+      <div className="team team-madrid">
+        <img src="/madrid_flag.png" alt="Real Madrid Flag" className="team-logo madrid-logo" />
+        <span className="team-name">Real Madrid</span>
       </div>
     </div>
+    
     <div className="match-stadium">
-    <p><i className="fas fa-calendar"></i> 7<sup>th</sup> May (Wednesday)</p>
-        <p><i className="fas fa-clock"></i> 12:30am - 2:00am (IST)</p>
       <h2>Venue: Palegaon Turf, Ambernath(E)</h2>
-        <p className="highlight">FIRST TIME EVER IN AMBERNATH!!</p>
+      
+      <div className="match-details-grid">
+  <p><FaCalendarAlt /> 11 May (Sunday)</p>
+  <p><FaClock /> 7:45pm - 10:00pm (IST)</p>
+  <p><FaDoorOpen /> Gates open at 7:00pm</p>
+</div>
+
+      
+      <p className="highlight">FIRST TIME EVER IN AMBERNATH!!</p>
       <p>Get ready for an unforgettable match under the lights!</p>
     </div>
   </section>
